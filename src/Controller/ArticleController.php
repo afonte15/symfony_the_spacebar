5255b6ace0e25a3bec757241a173df190771f922
Angie Fonteboa <fonteboa@Angies-MBP.fios-router.home>
2019-09-15 09:18:00 -0400

    installed the Route Annotation package for defining routes in the Controller files

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class ArticleController
+{
+	/**
+	 * @Route("/")
+	 */
+	public function homepage()
+	{
+		return new Response('MY FIRST SYMFONY PAGE!');
+	}
+
+
+
+}
