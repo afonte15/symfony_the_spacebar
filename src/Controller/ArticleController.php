77da789c8f49e51d1a620bd1f8d4ef66fd9ac0e9
Angie Fonteboa <fontebo@nyu.edu>
2019-09-29 22:28:26 -0400

    The Space Bar on header redirects to the homepage

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ArticleController extends AbstractController
 {
 	/**
-	 * @Route("/")
+	 * @Route("/", name="app_homepage")
 	 */
 	public function homepage()
 	{
